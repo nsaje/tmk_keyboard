fbcedd4243a70c6acf817bd61be7fb68bcde4bf4
Nejc Saje <nejc@saje.info>
2015-12-02 10:34:17 +0100

    nsaje layout

--- keyboard/ergodox/keymap.c
+++ keyboard/ergodox/keymap.c
@@ -85,6 +85,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "keymap_micro.h"
 #elif defined(KEYMAP_CUB)
 #include "keymap_cub.h"
+#elif defined(KEYMAP_CUB)
+#include "keymap_cub.h"
+#elif defined(KEYMAP_NSAJE)
+#include "keymap_nsaje.h"
 #else
 
 static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
---
+++ keyboard/ergodox/keymap_nsaje.h
@@ -0,0 +1,117 @@
+static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    KEYMAP(
+        // Left Hand
+        ESC, 1,   2,   3,   4,   5,   6,
+        TAB, Q,   W,   E,   R,   T,   LBRC,
+        LCTL,A,   S,   D,   F,   G,
+        LSFT,Z,   X,   C,   V,   B,   NO,
+        BSLS,GRV, LALT,FN1, LGUI,
+                                      LCTL,LALT,
+                                           HOME,
+                                  LSFT,BSPC, END,
+        // Right Hand
+            7,   8,   9,   0,   MINS,   EQL, BSPC,
+            RBRC, Y,   U,   I,   O,   P,   BSLS,
+                  H,   J,   K,   L,   SCLN,   ENT,
+            QUOT,  N,   M,   COMM,DOT, SLSH,   RSFT,
+                       SPC,DOWN, UP, RGHT,   RGUI,
+        RALT, RCTL,
+        PGUP,
+        PGDN, NO, ENT
+    ),
+
+    KEYMAP(  // Layer2: transparent on edges + hard-defined thumb keys, all others are empty
+        // left hand
+        FN0,NO,  NO,  NO,  NO,  NO,  NO,
+        TRNS,NO,  NO,  NO,  NO,  NO,  TRNS,
+        TRNS,NO,  NO,  NO,  NO,  NO,
+        TRNS,NO,  NO,  NO,  NO,  NO,  TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,
+                                      TRNS,TRNS,
+                                           TRNS,
+                                  TRNS,TRNS, TRNS,
+        // right hand
+             NO,  NO,  NO,  NO,  NO,  NO,  TRNS,
+             TRNS,NO,  NO,  NO,  NO,  NO,  TRNS,
+                  LEFT,  DOWN,  UP,  RGHT,  TRNS, TRNS,
+             TRNS,NO,  NO,  NO,  NO,  NO,  TRNS,
+                       TRNS,TRNS, TRNS, TRNS,   TRNS,
+        TRNS, TRNS,
+        TRNS,
+        TRNS, TRNS, TRNS
+    ),
+
+/*
+    // templates to copy from
+
+    KEYMAP(  // LayerN: transparent on edges + hard-defined thumb keys, all others are empty
+        // left hand
+        TRNS,NO,  NO,  NO,  NO,  NO,  NO,
+        TRNS,NO,  NO,  NO,  NO,  NO,  TRNS,
+        TRNS,NO,  NO,  NO,  NO,  NO,
+        TRNS,NO,  NO,  NO,  NO,  NO,  TRNS,
+        TRNS,TRNS,TRNS,LALT,LGUI,
+                                      TRNS,TRNS,
+                                           TRNS,
+                                 LCTL,LSFT,TRNS,
+        // right hand
+             NO,  NO,  NO,  NO,  NO,  NO,  TRNS,
+             TRNS,NO,  NO,  NO,  NO,  NO,  TRNS,
+                  NO,  NO,  NO,  NO,  NO,  TRNS,
+             TRNS,NO,  NO,  NO,  NO,  NO,  TRNS,
+                       RGUI,RALT,TRNS,TRNS,TRNS,
+        TRNS,TRNS,
+        TRNS,
+        TRNS,RSFT,RCTL
+    ),
+    KEYMAP(  // LayerN: fully transparent
+        // left hand
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,
+                                      TRNS,TRNS,
+                                           TRNS,
+                                 TRNS,TRNS,TRNS,
+        // right hand
+             TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+             TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+                  TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+             TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+                       TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,
+        TRNS,
+        TRNS,TRNS,TRNS
+    ),
+*/
+
+};
+
+/* id for user defined functions */
+enum function_id {
+    TEENSY_KEY,
+};
+
+// https://github.com/jcheng31/tmk_keyboard/blob/colemak/keyboard/ergodox/keymap_colemak.h
+static const uint16_t PROGMEM fn_actions[] = {
+    ACTION_FUNCTION(TEENSY_KEY), // Teensy
+    ACTION_LAYER_MOMENTARY(1), // Hold for Programming Layer
+};
+
+void action_function(keyrecord_t *event, uint8_t id, uint8_t opt)
+{
+    print("action_function called\n");
+    print("id  = "); phex(id); print("\n");
+    print("opt = "); phex(opt); print("\n");
+    if (id == TEENSY_KEY) {
+        clear_keyboard();
+        print("\n\nJump to bootloader... ");
+        _delay_ms(250);
+        bootloader_jump(); // should not return
+        print("not supported.\n");
+    }
+}
+
+
